3f0b876949897939c393df73e2288277715cf274
kelicaleb <kelicaleb7@gmail.com>
2022-11-15 10:35:19 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ const height = 74;
 const message = `${name} is ${height} inches tall`;
 
 module.exports = {name, height, message};
+const Name = "Susan"
+const Height = 39;
+const Message = `${Name} is ${Height} inches tall `;
+console.log(Message)
